3889ba70bbd5ae9082bce1d44c4de90b563c4eed
Beaudry Chase <beaudry.c.chase@dartmouth.edu>
2024-11-11 15:11:04 +0000

    updated tests to use files

--- day_template/test_solution.py
+++ day_template/test_solution.py
@@ -1,22 +1,5 @@
-from pathlib import Path
-
-from pytest import fixture, mark
-
 from day_template.solution import solution
-
-
-@fixture
-def testcase(request):
-    requestor_directory = Path(request.module.__file__).parent
-    with open(Path(requestor_directory, "tests", f"{request.param}.in")) as file:
-        input = file.read()
-    with open(Path(requestor_directory, "tests", f"{request.param}.out")) as file:
-        expected_out = file.read()
-    return (input, expected_out)
-
-
-def file_input(*file_names: str):
-    return mark.parametrize("testcase", file_names, indirect=True)
+from test_utils import file_input
 
 
 @file_input("a", "b")
