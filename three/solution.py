5dfe89cb642eee8981d3b83c6cdd80d4d940b47d
Beaudry Chase <beaudry.c.chase@dartmouth.edu>
2024-12-02 14:34:20 +0000

    init day 3

---
+++ three/solution.py
@@ -0,0 +1,19 @@
+from typing import Literal
+
+from .utils import input_multiline
+
+Sign = Literal[1, -1]
+
+
+def solution(input_value: str):
+    return ""
+
+
+def solution_two(input_value: str):
+    return ""
+
+
+if __name__ == "__main__":
+    input_value = input_multiline()
+    print(solution(input_value))
+    print(solution_two(input_value))
